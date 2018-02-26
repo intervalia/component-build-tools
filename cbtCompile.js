a8a08b1e0dc519c7d846ccbab2eadca5fe35e3c1
Mike Collins <intervalia@gmail.com>
2018-02-26 10:58:21 -0700

    Initial Release

---
+++ cbtCompile.js
@@ -0,0 +1,501 @@
+/* eslint brace-style: 0, spaced-comment: 0, no-multi-spaces: 0, object-property-newline: 0 */
+const fs = require('fs');
+const glob = require('glob');
+const path = require('path');
+const FILLER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 .-_!@#$%^&*=+';
+const FILLER_MAX = FILLER.length;
+const LOCALE_RE = /_(.*).json/;
+const MIXED_LANGS = [
+  '鼻毛', '指先', '眉毛', 'ひれ', 'ヘビ', 'カブ', '子供', '日本', '言語', '馬鹿', // Japanese Chars
+  '영어', '소금', '트럭', '히피', '포크', '토성', '아픈', '오리', '얼음', '극지', // Korean Chars
+  '孩子', '嬉皮', '雲彩', '占星', '胡說', '膀胱', '沙拉', '蠢貨', '烘烤', '蝸牛', // Chinese Chars
+  'да', 'ща', 'по', 'не', 'из', 'за', 'Ий', 'дя', 'ИФ', 'ья', // Russian Chars
+  'Ãé', 'Ûç', 'Çó', 'Ñá', 'Ýň', 'Èç', 'Ìë', 'Îú', 'Öà', 'Ūê' // Latin Chars
+];
+const MULTI_WS_RE = /\s+/g;
+const REPLACEMENT_MAP = {
+  A: 'ÀÁÂÃÄÅĀĄĂѦ', B: 'ƁɃḂ', C: 'ÇĆČĈĊ', D: 'ĎĐ', E: 'ÈÉÊËĒĘĚĔĖ', F: 'ƑḞ', G: 'ĜĞĠĢ',
+  H: 'ĤĦ', I: 'ÌÍÎÏĪĨĬĮİ', J: 'ĴɈ', K: 'ĶҞҠ', L: 'ŁĽĹĻĿ', M: 'ṀƜӍ', N: 'ÑŃŇŅŊПИ',
+  O: 'ÒÓÔÕÖØŌŐŎ', P: 'ƤṖ', R: 'ŔŘŖЯ', S: 'ŚŠŞŜȘ', T: 'ŤŢŦȚ', U: 'ÙÚÛÜŪŮŰŬŨŲЦ', V: 'ѴѶ',
+  W: 'ŴШЩѠ', X: 'ЖҲӾ', Y: 'ÝŶŸ', Z: 'ŹŽŻ',
+  a: 'àáâãäåāąă', b: 'БЪЬѢ', c: 'çćčĉċ', d: 'ďđ', e: 'èéêëēęěĕė', f: 'ƒḟ', g: 'ĝğġģ',
+  h: 'ĥħ', i: 'ìíîïīĩĭįı', j: 'ĵǰɉ', k: 'ķĸƙǩ', l: 'łľĺļŀ', m: 'ṁӎ', n: 'ñńňņŉŋ',
+  o: 'òóôõöøōőŏФ', r: 'ŕřŗя', s: 'śšşŝș', t: 'ťţŧț', u: 'ùúûüūůűŭũų', v: 'ѵѷ',
+  w: 'ŵѡ', x: 'ӿӽж', y: 'ýÿŷЧѰ', z: 'žżź'
+};
+const VALID_TEMPLATE_KEY_TEST_RE = /^[\w$][\w\d$]*$/;
+
+const readFile = filePath => fs.readFileSync(filePath, {'encoding': 'utf-8'}).trim().replace(/`/g, '\\`');
+
+/************************************************\
+                 Public Functions
+\************************************************/
+
+/*
+ * Compile the locale files into a single output string
+ * representing the contents of the file
+ */
+function locales(rootFolder, config) {
+  // Convert the list of globby locale filenames into a list of available filenames
+  const localeFileArray = getFileArrayFromGlob(rootFolder, config.localeFiles);
+
+  if (localeFileArray.length > 0) {
+    // If we have a list of locale files then process them
+    if (config.separateByLocale) {
+      // Return the processed results as an object of strings.
+      return createMultiLocaleFiles(rootFolder, localeFileArray, config);
+    }
+    else {
+      // Return the processed results as a single string.
+      return createSingleLocalesFile(rootFolder, localeFileArray, config);
+    }
+  }
+  else if (config.alwaysReturnFile) {
+    // If we are supposed to always return a file then return the
+    // default file contents as a string
+    return 'export default () => ({});';
+  }
+  else {
+    // Indicate that no files were found
+    return null;
+  }
+}
+
+/*
+ * Compile the tempalte files into a single output string
+ * representing the contents of the file
+ */
+function templates(rootFolder, config, localeList) {
+  // Convert the list of globby template filenames into a list of available filenames
+  const templateFileArray = getFileArrayFromGlob(rootFolder, config.templatefiles);
+
+  if (templateFileArray.length > 0) {
+    // If we have a list of template files then process them
+    if (localeList.length > 0) {
+      // Return the processed results as a single string.
+      return generateMultipleTemplates(rootFolder, templateFileArray, config, localeList);
+    }
+    else {
+      // Return the processed results as a single string.
+      return generateTemplateOutput(rootFolder, templateFileArray, config);
+    }
+  }
+  else if (config.alwaysReturnFile) {
+    // If we are supposed to always return a file then return the
+    // default file contents as a string
+    return 'export default {};';
+  }
+  else {
+    // Indicate that no template files were found
+    return null;
+  }
+}
+
+/************************************************\
+                 Helper Functions
+\************************************************/
+
+/*
+ * Convert an array of Globby paths into an array of paths of existing files.
+ */
+function getFileArrayFromGlob(rootFolder, globList) {
+  let globArray = globList;
+  if (!Array.isArray(globList)) {
+    globArray = [globList];
+  }
+  return globArray.reduce(
+    (obj, pattern) => {
+      const options = {
+        cwd: '/',
+        root: '/'
+      };
+
+      const tempPath = path.join(rootFolder, pattern);
+      const newList = glob.sync(tempPath, options);
+
+      // no files returned
+      if (!newList.length) {
+        // don't pass the file to the process script if it's a glob tempPath
+        if (!glob.hasMagic(tempPath)) {
+          obj.push(tempPath);
+        }
+      }
+      else {
+        newList.forEach(function(item) {
+          item = path.resolve(rootFolder, item);
+
+          if (obj.indexOf(item) === -1) {
+            obj.push(item);
+          }
+        });
+      }
+
+      return obj;
+    }, []
+  );
+}
+
+function readTranslations(rootFolder, fileList, config) {
+  const translations = fileList.reduce(
+    (obj, filePath) => {
+      let toks = filePath.match(LOCALE_RE);
+      let lang = toks[1];
+      let fileContents = readFile(filePath);
+      if (config.minTempalteWS) {
+        // Simple minimize of the white space
+        fileContents = fileContents.replace(MULTI_WS_RE, ' ');
+      }
+
+      var data;
+      obj[`${lang}filePath`] = filePath.replace(rootFolder, ''); // Save the filename of this locale file
+
+      try {
+        data = JSON.parse(fileContents); // Convert the file JSON text into an object
+        obj[lang] = data; // Save the JSON
+        obj.langs.push(lang); // Indicate that we support this language
+
+        if (lang === config.defaultLocale) {
+          // If we are processing the default locale then save off the current set of keys.
+          obj.keys = Object.keys(data);
+        }
+      }
+      catch (e) {
+        throw new Error(`Unable to parse locale file: ${filePath}:: ${e}`);
+      }
+
+      return obj;
+    }, {langs: []}
+  );
+
+  if (config.addEOLocale && !translations.eo) {
+    // If we are adding EO and there was not EO file then generate a version of EO
+    translations.eo = createEOTranslations(translations[config.defaultLocale]); // Save the JSON
+    translations.langs.push('eo'); // Indicate that we support this language
+  }
+
+  if (!translations.keys) {
+    // If there was no locale file for the default locale then flag an error
+    throw new Error(`locale file for default locale "${config.defaultLocale}" was not found.`);
+  }
+
+  return translations;
+}
+
+/*
+ * Generate an array of locale file contents for every locale source file
+ */
+function createMultiLocaleFiles(rootFolder, fileList, config) {
+  const contents = {};
+
+  // Read all of the locale file
+  let translations = readTranslations(rootFolder, fileList, config);
+
+  // Build up the contents object. Each entry will contain the contents of the `locales_??.mjs` file to create.
+  translations.langs.forEach(
+    (locale) => {
+      var strings = {};
+      translations.keys.forEach(
+        (key, keyIndex) => {
+          if (config.tagMissingStrings) {
+            strings[key] = translations[locale][key] || `-*${translations[config.defaultLocale][key]}*-`;
+          }
+          else {
+            strings[key] = translations[locale][key] || translations[config.defaultLocale][key];
+          }
+        }
+      );
+
+      contents[locale] = `// This is an auto generated file. Do not edit!
+export default function () (${JSON.stringify(strings)});
+`;
+    }
+  );
+
+  return contents;
+}
+
+/*
+ * Generate locale file contents that includes every locale source file
+ */
+function createSingleLocalesFile(rootFolder, fileList, config) {
+  // Read all of the locale file
+  let translations = readTranslations(rootFolder, fileList, config);
+
+  let len = translations.langs.length - 2;
+  // Build up the file content for the file `locales.mjs`
+  let strings = translations.langs.sort().reduce(
+    (str, locale, index) => {
+      let filePath = translations[`${locale}filePath`];
+      str +=  `  // Included locale file: .${filePath}\n`;
+      const strings = [];
+
+      translations.keys.forEach(
+        (key, keyIndex) => {
+          if (config.tagMissingStrings) {
+            strings.push(translations[locale][key] || `-*${translations[config.defaultLocale][key]}*-`);
+          }
+          else {
+            strings.push(translations[locale][key] || translations[config.defaultLocale][key]);
+          }
+        }
+      );
+
+      str += `  "${locale}": ${JSON.stringify(strings)}${(index > len) ? '\n' : ',\n'}`;
+
+      return str;
+    }, '{\n'
+  ) + '}';
+
+  return `// This is an auto generated file. Do not edit!
+const strCache = {};
+const strKeys = ${JSON.stringify(translations.keys)};
+const strs = ${strings};
+
+function getLocaleStrings(locale = window.locale) {
+  // If the requested locale is not supported the use the default locale
+  if (!strs[locale]) {
+    locale = locale.split('-')[0];
+    if (!strs[locale]) {
+      locale = '${config.defaultLocale}';
+    }
+  }
+
+  if (!strCache[locale]) {
+    // If this locale isn't already in the cache, then create it and store it in the cache
+    strCache[locale] = strKeys.reduce((obj, key, i) => { obj[key] = locale === 'ke' ? key : strs[locale][i]; }, {});
+  }
+
+  // Return the cached language strings
+  return strCache[locale];
+}
+
+export default getLocaleStrings;
+`;
+}
+
+function generateMultipleTemplates(rootFolder, templateFiles, config, localeList) {
+  throw new Error('Not implamented yet.');
+  /*
+  const results = {};
+
+  localeList.forEach(
+    (locale) => {
+      var {localesName} = config.tempLocalesName;
+      localesName = localesName.replace('.mjs', `${locale}.mjs`);
+      const len = templateFiles.length - 2;
+      let templateOutputStr = templateFiles.reduce(
+        (str, filePath, i) => {
+          // localeList = array of locales that are to be processed
+          // locale = current locale we are processing
+          // localesName = name of locale file to import into template file.
+          // templateFiles = array of template files to include
+          // str = templateCode we are producing
+          // filePath = file path of current template file
+          // i = index into templateFiles array
+          let templateStr = readFile(filePath);
+          let code = `var ret;with(this){try{ret=(${property})}catch(ex){}return ret}`;
+
+          try {
+            getValFn = new Function('lang, $index', code); // eslint no-new-func: 0
+          }
+
+          catch(ex) {
+            console.error(`Exception while trying to process template "${filePath}".`);
+            console.error('Code:', code);
+            throw ex;
+          }
+
+          const newFn = new Function();
+          if (config.minTempalteWS) {
+            templateStr = templateStr.replace(MULTI_WS_RE, ' ');
+          }
+          let ext = path.extname(filePath);
+          let templateKey = path.basename(filePath, ext);
+          if (!VALID_TEMPLATE_KEY_TEST_RE.test(templateKey)) {
+            throw new Error(`Invalid Template name: ${filePath}\nTemplate file names can only use $, _ or alphanumeric characters.`);
+          }
+
+          let comma = i > len ? '' : ',';
+          let tempPath = filePath.replace(rootFolder, '');
+
+          str += `  // Included template file: .${tempPath}\n`;
+          str += `  '${templateKey}': getter(\`${templateStr}\`)${comma}\n`;
+
+          return str;
+        }, '');
+    }
+  );
+
+  return results;
+  */
+}
+
+function generateTemplateOutput(rootFolder, templateFiles, config) {
+  var outputStr = `// This is an auto generated file. Do not edit!
+import locales from './${config.tempLocalesName}';
+const lang = locales();
+const templates = {};
+
+const getter = str => {
+  var el = document.createElement('template');
+  el.innerHTML = str;
+
+  return {
+    value: el.content.cloneNode(true)
+  };
+};
+
+Object.defineProperties(templates, {
+`;
+
+  const len = templateFiles.length - 2;
+  outputStr = templateFiles.reduce(
+    (str, filePath, i) => {
+      let templateStr = readFile(filePath);
+      if (config.minTempalteWS) {
+        templateStr = templateStr.replace(MULTI_WS_RE, ' ');
+      }
+      let ext = path.extname(filePath);
+      let templateKey = path.basename(filePath, ext);
+      if (!VALID_TEMPLATE_KEY_TEST_RE.test(templateKey)) {
+        throw new Error(`Invalid Template name: ${filePath}\nTemplate file names can only use $, _ or alphanumeric characters.`);
+      }
+
+      let comma = i > len ? '' : ',';
+      let tempPath = filePath.replace(rootFolder, '');
+
+      str += `  // Included template file: .${tempPath}\n`;
+      str += `  '${templateKey}': getter(\`${templateStr}\`)${comma}\n`;
+
+      return str;
+    }, outputStr
+  ) + `});
+
+export default templates;
+`;
+
+  return outputStr;
+}
+
+/*
+ * creates pseudo locale object from one english locale object
+ */
+function createEOTranslations(engProps) {
+  return Object.keys(engProps).reduce(
+    (obj, key) => {
+      obj[key] = makeEOProp(key, engProps[key]);
+      return obj;
+    }, {}
+  );
+}
+
+/*
+ * converts characters, adds length, and adds CKJ characters for a single string in a locale
+ */
+function makeEOProp(key, enStr) {
+  const keyHashCode = getKeyHash(key);
+  let newValue = convertString(keyHashCode, enStr);
+  let suffix = MIXED_LANGS[keyHashCode % MIXED_LANGS.length];
+  let length = newValue.length;
+  let combinedLength = length + suffix.length;
+
+  if (length > 0 && length <= 5) {
+    length = 9;
+  }
+  else if (length >= 6 && length <= 25) {
+    length *= 1.9;
+  }
+  else if (length >= 26 && length <= 40) {
+    length *= 1.6;
+  }
+  else if (length >= 41 && length <= 70) {
+    length *= 1.3;
+  }
+
+  let expansion = createFiller(Math.round(length - combinedLength));
+
+  return '[' + newValue + expansion + suffix + ']';
+}
+
+/*
+ * performs the character replacement for pseudo locale creation
+ */
+function convertString(keyHashCode, str) {
+  let i;
+  let isInTag = false;
+  let isInVar = false;
+  let strLength = str.length;
+  let ret = '';
+
+  for (i = 0; i < strLength; i++) {
+    let current = str[i];
+
+    if (isInTag) {
+      ret += current;
+      isInTag = current !== '>';
+      continue;
+    }
+    else if (isInVar) {
+      ret += current;
+      isInVar = current !== '}';
+      continue;
+    }
+    else if (current === '<') {
+      // ignore HTML tags (but not content inside opening and closing tags,
+      // e.g. for <p>Something</p> ignores <p> and </p> but not "Something")
+      ret += current;
+      isInTag = true;
+      continue;
+    }
+    else if (current === '$' && str[i + 1] === '{') {
+      // ignore replacement variables, e.g. ${myVar}
+      ret += current;
+      isInVar = true;
+      continue;
+    }
+
+    var replacements = REPLACEMENT_MAP[current] || [current];
+    ret += replacements[keyHashCode % replacements.length];
+  }
+
+  return ret;
+}
+
+/*
+ * Create a hash based on the string's KEY
+ *
+ * str = string to use for the hash
+ */
+function getKeyHash(str = '') {
+  let hash = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    hash = ((hash << 5) - hash) + str.charCodeAt(i);
+    hash = hash & hash; // Convert to 32bit integer
+  }
+
+  return Math.abs(hash);
+}
+
+/*
+ * Create filler to bring the EO strings up to a percentage longer
+ *
+ * count = number of characters to include
+ */
+function createFiller(count) {
+  var fill = '';
+
+  if (count > 0) {
+    fill = '-';
+    for (let i = 1; i < count; i++) {
+      fill += FILLER[Math.round(Math.random() * FILLER_MAX)];
+    }
+  }
+
+  return fill + '-:';
+}
+
+module.exports = {
+  locales,
+  templates
+};
---
+++ rollup.root.config.js
@@ -0,0 +1,191 @@
+/* eslint brace-style: 0, spaced-comment: 0, no-multi-spaces: 0 */
+const fs = require('fs');
+const fscompile = require('./fscompile');
+const glob = require('glob');
+const path = require('path');
+const PLUGIN_BUBLE = require('rollup-plugin-buble')({ transforms: { dangerousTaggedTemplateString: true } });
+const PLUGIN_UGLIFY = require('rollup-plugin-uglify-es')();
+const ROOT = process.cwd();
+const BUILD_TYPES = {
+  IIFE: 'iife', // IFFE output
+  IIFE5: 'iife5', // IFFE output with ES5 transpile
+  CJS: 'cjs', // Common JS (require)
+  CJS5: 'cjs5', // Common JS (require) with ES5 transpile
+  MJS: 'es' // ES6 Modules (import)
+};
+const globOptions = { cwd: '/', root: '/' };
+const getSrcFiles = (...pathParts) => glob.sync(path.resolve(...pathParts), globOptions);
+
+function init(theirConfig = {}) {
+  const rollupOptionsArray = [];
+  const config = Object.assign({
+    addEOLocale: true, // Add the EO locale if it does not exist
+    alwaysReturnFile: true,
+    buildTypes: [ BUILD_TYPES.MJS, BUILD_TYPES.IIFE ],
+    defaultLocale: 'en',
+    distPath: 'dist/js', // Path into which the distribution files will be placed
+    includePath: true, // Place the dist files inside their folder
+    localeFiles: ['locales/strings_*.json'],
+    minTempalteWS: true, // Minimize the white space for templates
+    separateByLocale: false,
+    //srcFileName: 'wc.*.mjs',
+    srcFolders: [], // Where to look for source files. User must supply this
+    tagMissingStrings: true, // Mark missing locale strings so they are easily seen
+    templatefiles: ['*.html'],
+    tempLocalesName: 'locales.mjs',
+    tempPath: './_compiled/',
+    tempTemplateName: 'templates.mjs'
+  }, theirConfig);
+
+  config.srcFolders.forEach(
+    temp => {
+      const srcList = [];
+      const srcRoot = path.join(ROOT, temp);
+      const label = `Processing Locales and Templates for ${temp}`;
+
+      console.time(label);
+      fs.readdirSync(srcRoot).forEach(
+        (tempPath) => {
+          let fname = config.srcFileName || `${tempPath}.mjs`;
+          let componentFile = getSrcFiles(srcRoot, tempPath, fname)[0];
+
+          if (componentFile && fs.existsSync(componentFile)) {
+            srcList.push({
+              srcPath: tempPath
+            });
+
+            let localeList = [];
+            let componentPath = path.join(srcRoot, tempPath);
+            // TODO: If we are compiling for each locale then we need to repeat the `srcList.push`
+            let localeCode = fscompile.locales(componentPath, config);
+            if (typeof localeCode === 'object') {
+              localeList = Object.keys(localeCode);
+              localeList.forEach(
+                (key) => {
+                  writeFile(path.join(componentPath, config.tempPath, config.tempLocalesName.replace('.mjs', `_${key}.mjs`)), localeCode[key]);
+                }
+              );
+            }
+            else {
+              writeFile(path.join(componentPath, config.tempPath, config.tempLocalesName), localeCode);
+            }
+
+            let templateCode = fscompile.templates(componentPath, config, localeList);
+            writeFile(path.join(componentPath, config.tempPath, config.tempTemplateName), templateCode);
+          }
+        }
+      );
+      console.timeEnd(label);
+
+      srcList.forEach(
+        ({ varName, srcPath, srcFile = config.srcFileName || srcPath + '.mjs', buildTypes }, index) => {
+          if (!srcPath) {
+            throw new Error('`srcPath` must be defined at index ' + index);
+          }
+          if (!srcFile) {
+            throw new Error('`srcFile` must be defined at index ' + index);
+          }
+
+          const input = getSrcFiles(srcRoot, srcPath, srcFile)[0];
+
+          config.buildTypes.forEach(
+            buildType => {
+              let format = buildType;
+              let transpile = false;
+              let dstName = srcPath;
+              if (buildType === BUILD_TYPES.IIFE5) {
+                dstName += '.iife5.js';
+                transpile = true;
+                format = BUILD_TYPES.IIFE;
+                if (!varName) {
+                  varName = srcPath.replace(/[.-]/g, '_');
+                }
+              }
+              else if (buildType === BUILD_TYPES.IIFE) {
+                dstName += '.iife.js';
+                if (!varName) {
+                  varName = srcPath.replace(/[.-]/g, '_');
+                }
+              }
+              else if (buildType === BUILD_TYPES.CJS5) {
+                dstName += '.cjs5.js';
+                transpile = true;
+                format = BUILD_TYPES.CJS;
+              }
+              else if (buildType === BUILD_TYPES.CJS) {
+                dstName += '.cjs.js';
+              }
+              else {
+                dstName += '.mjs';
+              }
+
+              var outputPath = config.distPath || 'dist';
+              var file = config.includePath ? path.resolve(ROOT, outputPath, srcPath, dstName) : path.resolve(ROOT, outputPath, dstName);
+
+              const buildItem = {
+                input,
+                plugins: [],
+                output: {
+                  file,
+                  format,
+                  name: varName
+                }
+              };
+
+              if (transpile) {
+                buildItem.plugins.push(PLUGIN_BUBLE);
+              }
+              rollupOptionsArray.push(buildItem);
+
+              // TODO: Add option to allow or stop minify
+              const buildItem2 = Object.assign({}, buildItem);
+              buildItem2.plugins = [];
+              if (transpile) {
+                buildItem2.plugins.push(PLUGIN_BUBLE);
+              }
+              buildItem2.plugins.push(PLUGIN_UGLIFY);
+              buildItem2.output = Object.assign({}, buildItem.output);
+              buildItem2.output.file = buildItem.output.file.replace(/(.[a-z]{2,3}$)/, '.min$1');
+              rollupOptionsArray.push(buildItem2);
+            }
+          );
+        }
+      );
+    }
+  );
+
+  return rollupOptionsArray;
+}
+
+function createFolders(filePath) {
+  let pathParts = filePath.split(path.sep);
+  let len = pathParts.length;
+  let tempPath = path.sep + pathParts[0];
+  for (let i = 1; i < len; i++) {
+    if (!fs.existsSync(tempPath)) {
+      fs.mkdirSync(tempPath);
+    }
+
+    tempPath = path.join(tempPath, pathParts[i]);
+  }
+}
+
+function writeFile(filePath, content) {
+  createFolders(filePath);
+
+  let bakFilePath = `${filePath}.bak`;
+
+  if (fs.existsSync(bakFilePath)) {
+    fs.unlinkSync(bakFilePath);
+  }
+  if (fs.existsSync(filePath)) {
+    fs.renameSync(filePath, bakFilePath);
+  }
+
+  fs.writeFileSync(filePath, content);
+}
+
+module.exports = {
+  init,
+  BUILD_TYPES
+};
